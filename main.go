e29fd8c2e6f6b82a072a438716e1777da90dd8fb
Andrew Meissner <ameissner@sofi.org>
2019-04-12 11:26:42 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+const environment = "dev"
+
+type binaryFileSystem struct {
+	fs http.FileSystem
+}
+
+func (b *binaryFileSystem) Open(name string) (http.File, error) {
+	return b.fs.Open(name)
+}
+
+func (b *binaryFileSystem) Exists(prefix, filepath string) bool {
+	if p := strings.TrimPrefix(filepath, prefix); len(p) > len(filepath) {
+		if _, err := b.fs.Open(p); err != nil {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+// BinaryFileSystem ...
+func BinaryFileSystem(root string) *binaryFileSystem {
+	return &binaryFileSystem{
+		fs: &assetfs.AssetFS{
+			Asset:     Asset,
+			AssetDir:  AssetDir,
+			AssetInfo: AssetInfo,
+			Prefix:    root,
+		},
+	}
+}
+
+func main() {
+	router := gin.Default()
+
+	if environment == "dev" {
+		router.Use(static.Serve("/", static.LocalFile("ui/build", true)))
+	} else {
+		router.StaticFS("/", BinaryFileSystem("ui/build"))
+	}
+
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	router.Run(":6969")
+}
